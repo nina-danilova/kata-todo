c5283e43a5d5d18e3bf8820574e88fbbd8076f7e
Mau-Mau <nina.mau.nsk@gmail.com>
2023-02-02 18:12:02 +0700

    Добавляет верстку в проект. Добавляет верхний компонент приложения

---
+++ src/components/app/app.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <section className="todo-app">
+      <header className="header">
+        <h1>todos</h1>
+        <input className="new-todo" placeholder="What needs to be done?" autoFocus />
+      </header>
+      <section className="main">
+        <ul className="todo-list">
+          <li className="completed">
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>
+                <span className="description">Completed task</span>
+                <span className="created">created 17 seconds ago</span>
+              </label>
+              <button className="icon icon-edit"></button>
+              <button className="icon icon-destroy"></button>
+            </div>
+          </li>
+          <li className="editing">
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>
+                <span className="description">Editing task</span>
+                <span className="created">created 5 minutes ago</span>
+              </label>
+              <button className="icon icon-edit"></button>
+              <button className="icon icon-destroy"></button>
+            </div>
+            <input type="text" className="edit" placeholder="Editing task" />
+          </li>
+          <li>
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>
+                <span className="description">Active task</span>
+                <span className="created">created 5 minutes ago</span>
+              </label>
+              <button className="icon icon-edit"></button>
+              <button className="icon icon-destroy"></button>
+            </div>
+          </li>
+        </ul>
+        <footer className="footer">
+          <span className="todo-count">1 items left</span>
+          <ul className="filters">
+            <li>
+              <button className="selected">All</button>
+            </li>
+            <li>
+              <button>Active</button>
+            </li>
+            <li>
+              <button>Completed</button>
+            </li>
+          </ul>
+          <button className="clear-completed">Clear completed</button>
+        </footer>
+      </section>
+    </section>
+  );
+}
+
+export default App;
