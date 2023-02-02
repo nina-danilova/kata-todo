a5cc2ba84bf8af6256b26c783db88765e6fd4b0f
Mau-Mau <nina.mau.nsk@gmail.com>
2023-02-02 18:15:04 +0700

    Добавляет все стили приложения

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+root.render(<App />);
 
